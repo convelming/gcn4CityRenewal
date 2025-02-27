5c8e173636587f91cd522f7f947aacb177ee06c2
convelming <convel@163.com>
2025-02-27 18:16:47 +0800

    draw target area on osm to get the area's polygon coords

--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ from src.utils import OsmUtils
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     rangeFile = "/Users/convel/Desktop/wenchongDst.geojson"
     # x, y, m, n = [113.43796, 23.08660, 113.51140, 23.13701]
     # OsmUtils.downloadOsmByBoundary(x,y,m,n,"/Users/convel/Desktop/wenchongDst.osm")
